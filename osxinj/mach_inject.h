9a93dc80f6a9661c8d76cd58e57b46060455af38
Stanley Cen <jiecenzhao@gmail.com>
2013-08-08 13:36:59 -0700

    Fix code formatting

--- osxinj/mach_inject.h
+++ osxinj/mach_inject.h
@@ -2,7 +2,7 @@
 //   Copyright (c) 2003-2012 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
 //   Some rights reserved: http://opensource.org/licenses/mit
 //   https://github.com/rentzsch/mach_inject
-	
+
 #ifndef		_mach_inject_
 #define		_mach_inject_
 
@@ -17,54 +17,54 @@
 #define	INJECT_ENTRY_SYMBOL	"injectEntry"
 
 typedef	void	(*mach_inject_entry)( ptrdiff_t codeOffset, void *paramBlock,
-				size_t paramSize, void* dummy_pthread_data );
+size_t paramSize, void* dummy_pthread_data );
 
 __BEGIN_DECLS
-	
+
 /*******************************************************************************
-	Starts executing threadEntry in a new thread in the process specified by
-	targetProcess.
-	
-	@param	threadEntry		->	Required pointer to injected thread's entry
-								point.
-	@param	paramBlock		->	Optional pointer to block of memory to pass to
-								the injected thread.
-	@param	paramSize		->	Optional size of paramBlock.
-	@param	targetProcess	->	Required target process ID.
-	@param	stackSize		->	Optional stack size of threadEntry's thread. Set
-								to zero for default (currently 8K usable).
-	@result					<-	mach_error_t
+ Starts executing threadEntry in a new thread in the process specified by
+ targetProcess.
+ 
+ @param	threadEntry		->	Required pointer to injected thread's entry
+ point.
+ @param	paramBlock		->	Optional pointer to block of memory to pass to
+ the injected thread.
+ @param	paramSize		->	Optional size of paramBlock.
+ @param	targetProcess	->	Required target process ID.
+ @param	stackSize		->	Optional stack size of threadEntry's thread. Set
+ to zero for default (currently 8K usable).
+ @result					<-	mach_error_t
+ 
+ ***************************************************************************/
 
-	***************************************************************************/
-
-	mach_error_t
+mach_error_t
 mach_inject(
-		const mach_inject_entry	threadEntry,
-		const void				*paramBlock,
-		size_t					paramSize,
-		pid_t					targetProcess,
-		vm_size_t				stackSize );
+            const mach_inject_entry	threadEntry,
+            const void				*paramBlock,
+            size_t					paramSize,
+            pid_t					targetProcess,
+            vm_size_t				stackSize );
 
 /*******************************************************************************
-	Given a pointer, returns its Mach-O image and image size.
-	
-	@param	pointer			->	Required pointer.
-	@param	image			<-	Optional returned pointer to image (really a
-								mach_header).
-	@param	size			<-	Optional returned size of the image.
-	@param  jumpTableOffset <-  Optional returned offset of jump table within image (useful on intel)
-	@param  jumpTableSize	<-  Optional returned size of jump table (useful on intel)
-	@result					<-	mach_error_t
-	
-	***************************************************************************/
+ Given a pointer, returns its Mach-O image and image size.
+ 
+ @param	pointer			->	Required pointer.
+ @param	image			<-	Optional returned pointer to image (really a
+ mach_header).
+ @param	size			<-	Optional returned size of the image.
+ @param  jumpTableOffset <-  Optional returned offset of jump table within image (useful on intel)
+ @param  jumpTableSize	<-  Optional returned size of jump table (useful on intel)
+ @result					<-	mach_error_t
+ 
+ ***************************************************************************/
 
-	mach_error_t
+mach_error_t
 machImageForPointer(
-		const void *pointer,
-		const void **image,
-		unsigned long *size,
-		unsigned int *jumpTableOffset,
-		unsigned int *jumpTableSize );
+                    const void *pointer,
+                    const void **image,
+                    unsigned long *size,
+                    unsigned int *jumpTableOffset,
+                    unsigned int *jumpTableSize );
 
 __END_DECLS
 #endif	//	_mach_inject_
