5744e08d790a2e22bc3b695ddc9db3f8b4a5fbe4
Stanley Cen <jiecenzhao@gmail.com>
2015-03-20 17:24:27 -0400

    add check for root access
    injection fails without root

--- osxinj/main.cpp
+++ osxinj/main.cpp
@@ -5,13 +5,20 @@
 #include <cstring>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
-        fprintf(stderr, "Usage: osxinj [proc_name] [lib]\n");
-        return 0;
+        fprintf(stderr, "Usage: ./osxinj [proc_name] [lib]\n");
+        return -1;
+    }
+    
+    if (getuid() > 0) {
+        fprintf(stderr, "please run me as root\n");
+        return -1;
     }
 
     char path[4096];
